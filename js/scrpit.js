d895787313496d23ccadd52a072c5c9b985c8190
Mohammed AlMuhanna <mhaam@arlafoods.com>
2023-04-12 12:16:37 +0300

    Changes to be committed: 	modified:   index.html 	modified:   js/scrpit.js 	new file:   test.html

--- js/scrpit.js
+++ js/scrpit.js
@@ -7,7 +7,6 @@ function overlayText() {
       let img = new Image(),
             canvas = document.getElementById("demo"),
             ctx = canvas.getContext("2d");
-      // img.crossOrigin = "anonymous";
       img.onload = () => {
             canvas.width = img.naturalWidth;
             canvas.height = img.naturalHeight;
@@ -24,23 +23,9 @@ function overlayText() {
                         console.error(err);
                   }
             );
-            // link.href = document.getElementById('demo').toDataURL();
       };
       
       img.src = "images/img.jpg";
-      var link = document.createElement('a');
-      link.innerHTML = 'Save';
-      link.addEventListener('click', function(e) {link.download = "imagename.png";}, false);
-      document.body.appendChild(link);
-      link.click(); 
-
-      
-      // html2canvas(document.querySelector("#demo")).then(canvas => {canvas.toBlob(function(blob) {window.saveAs(blob, 'my_image.jpg');});});
-      // let a = document.createElement('a');
-      // a.href = canvas.toDataURL("image/png");
-      // a.download = "EidAlFitrGreeting.png";
-      // document.body.appendChild(a);
-      // a.click(); 
 }
 
 function downloadURI(uri, name) {
@@ -53,8 +38,6 @@ function downloadURI(uri, name) {
 saveButton.addEventListener('click', function (event) {
       event.preventDefault();
       html2canvas(document.getElementById("demo"), {
-            useCORS: true,
-            allowTaint: true,
             onrendered: function (canvas) {
                   var myImage = canvas.toDataURL("image/png");
                   // var myImage = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
