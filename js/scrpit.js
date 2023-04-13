499d011eebbf97abd6c2364b5801a9d235c87af4
Mohammed AlMuhanna <mhaam@arlafoods.com>
2023-04-13 13:16:06 +0300

    Changes to be committed: 	modified:   js/scrpit.js

--- js/scrpit.js
+++ js/scrpit.js
@@ -7,14 +7,14 @@ function overlayText() {
       img.onload = () => {
             canvas.width = img.naturalWidth;
             canvas.height = img.naturalHeight;
-            ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+            ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight); 
             fontTajawal.load().then(
                   () => {
-                        ctx.font = '700 36px "Tajawal"';
+                        ctx.font = '700 36px "Tajawal"';                   
                         ctx.fillStyle = "rgba(29,123,48, 1)";
                         ctx.textAlign = "center";
                         ctx.textBaseline = "middle";
-                        ctx.fillText(document.getElementById("name").value, 833, 657, 500);
+                        ctx.fillText(document.getElementById("name").value, 836, 659, 500);
                   },
                   (err) => {
                         console.error(err);
