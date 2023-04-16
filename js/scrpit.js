0fa1f1e439dfaab5dcdf6caf3e917da6c6846b9b
Mohammed AlMuhanna <mhaam@arlafoods.com>
2023-04-16 10:04:50 +0300

    modified:   images/EidAlFitrGreeting.jpg 	deleted:    images/img.jpg 	modified:   js/scrpit.js

--- js/scrpit.js
+++ js/scrpit.js
@@ -11,13 +11,13 @@ function overlayText() {
             fontTajawal.load().then(
                   () => {
                         ctx.textBaseline = "middle";
-                        ctx.font = '700 36px "Tajawal"';
+                        ctx.font = '700 56px "Tajawal"';
                         ctx.textAlign = "center";
                         var width = ctx.measureText(document.getElementById("name").value).width;
-                        ctx.fillStyle = "rgba(255, 255, 255, 0.35)";
-                        ctx.fillRect(836-width / 2 - 10, 659-30, width + 20, 50);// + 20 since the text begins at 10
-                        ctx.fillStyle = "rgba(29,123,48, 1)";
-                        ctx.fillText(document.getElementById("name").value, 836, 659, 500);
+                        ctx.fillStyle = "rgba(255, 255, 255, 0.4)";
+                        ctx.fillRect(1045-width / 2 - 10, 885-30, width + 20, 50);// + 20 since the text begins at 10
+                        ctx.fillStyle = "rgba(0,155,19,1)";
+                        ctx.fillText(document.getElementById("name").value, 1045, 885, 1045);
                   },
                   (err) => {
                         console.error(err);
@@ -33,3 +33,4 @@ download_img = function (el) {
       var image = canvas.toDataURL("image/jpg");
       el.href = image;
 };
+
